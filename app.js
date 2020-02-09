1aba5d4ca46f615ddc10202a21c8474d715df84f
rahimatunnimah <rahimatunnimah@gmail.com>
2020-02-10 01:24:57 +0700

    err

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.SERVER_PORT || 5000
+
 const router = require('./src/routers/index')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.use('/api/v1', router)
-app.listen(port, function(){
+app.listen(port, function(err){
+    if (err) throw err
     console.log('app running')
 })
