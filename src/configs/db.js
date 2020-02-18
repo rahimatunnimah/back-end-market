38d096160032316d30e093cb5e7b9500efab8728
rahimatunnimah <rahimatunnimah@gmail.com>
2020-02-18 23:48:05 +0700

    dotenv

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+require('dotenv').config();
 const mysql = require('mysql')
 const connection = mysql.createConnection({
     host : process.env.DB_HOST,
